5c38cd663a73d9d3491fe4648126746b429502fb
Sublimeful <Qiangwu2000@gmail.com>
2025-02-11 12:35:50 -0600

    Updated next.js and added github link

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,21 @@ export default function RootLayout({
       <body className="w-screen h-screen bg-white dark:bg-black text-black dark:text-white">
         <div className="w-full h-full grid grid-rows-[4rem_auto] grid-cols-[24rem_auto]">
           {/* Navbar */}
-          <div className="col-span-2 border-b border-black dark:border-white" />
+          <div className="col-span-2 border-b flex flex-row items-center border-black dark:border-white">
+            <a
+              className="w-12 h-12 ml-2 rounded-full overflow-clip"
+              href="https://github.com/Sublimeful"
+            >
+              <img src="https://avatars.githubusercontent.com/u/42013583" />
+            </a>
+            <a className="ml-3" href="https://github.com/Sublimeful">
+              Sublimeful
+            </a>
+          </div>
           {/* Sidebar */}
           <div className="border-r border-black dark:border-white" />
           {/* Content */}
-          {children}
+          <div className="w-full h-full overflow-y-auto">{children}</div>
         </div>
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 export default function Page() {
-  return <div className="w-full h-full overflow-y-auto">
-    <h1 className="font-bold text-4xl">Hello, my name is Jian Qiang Wu...</h1>
-    <h1 className="font-bold text-4xl">This is my homepage</h1>
-  </div>
+  return (
+    <>
+      <h1 className="font-bold text-4xl">Hello, my name is Jian Qiang Wu...</h1>
+      <h1 className="font-bold text-4xl">This is my homepage</h1>
+    </>
+  );
 }
