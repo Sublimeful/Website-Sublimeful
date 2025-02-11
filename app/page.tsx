bcdf353188ea58afb98f4c3988924813a0563ef3
Sublimeful <Qiangwu2000@gmail.com>
2025-02-11 00:15:57 -0600

    Added a homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,6 @@
 export default function Page() {
-  return <p>Dashboard Page</p>
+  return <div className="w-full h-full overflow-y-auto">
+    <h1 className="font-bold text-4xl">Hello, my name is Jian Qiang Wu...</h1>
+    <h1 className="font-bold text-4xl">This is my homepage</h1>
+  </div>
 }
