152e4d4baded37351873203b32e2d081097ab30c
Sublimeful <Qiangwu2000@gmail.com>
2025-02-16 01:14:28 -0600

    Changed scrollbar; Moved links to navbar instead

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,25 +20,26 @@ export default function RootLayout({
         />
       </head>
       <body className="w-screen h-screen bg-white dark:bg-black text-black dark:text-white">
-        <div className="w-full h-full grid grid-rows-[4rem_auto] grid-cols-[16rem_auto]">
+        <div className="w-full h-full grid grid-rows-[4rem_auto]">
           {/* Navbar */}
-          <div className="col-span-2 border-b flex flex-row items-center border-black dark:border-white">
-            <a
-              className="w-12 h-12 ml-2 rounded-full overflow-clip"
-              href="https://github.com/Sublimeful"
-            >
-              <img src="https://avatars.githubusercontent.com/u/42013583" />
-            </a>
-            <a className="ml-3" href="https://github.com/Sublimeful">
-              Sublimeful
-            </a>
-          </div>
-          {/* Sidebar */}
-          <div className="flex flex-col [&_a]:before:content-['\00a0>_'] border-r border-black dark:border-white">
-            <a href="/">Homepage</a>
-            <a href="/about">About me</a>
-            <a href="/projects">Projects</a>
-            <a href="/blog">Random stuff</a>
+          <div className="flex flex-row justify-between items-center">
+            <div className="flex flex-row gap-4 ml-4 items-center">
+              <a
+                className="w-10 h-10 rounded-full overflow-clip"
+                href="https://github.com/Sublimeful"
+              >
+                <img src="https://avatars.githubusercontent.com/u/42013583" />
+              </a>
+              <a href="https://github.com/Sublimeful">
+                Sublimeful
+              </a>
+            </div>
+            <div className="flex flex-row gap-10 mr-5">
+              <a href="/">Homepage</a>
+              <a href="/about">About me</a>
+              <a href="/projects">Projects</a>
+              <a href="/blog">Random stuff</a>
+            </div>
           </div>
           {/* Content */}
           <div className="w-full h-full overflow-y-auto">{children}</div>
