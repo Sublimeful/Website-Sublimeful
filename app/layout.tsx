ba531b20d6cfc1be3998be53cf6dfd4040e87a28
Sublimeful <Qiangwu2000@gmail.com>
2025-02-16 04:45:10 -0600

    Set default to dark theme to not flashbang anyone on first visit. Fixed some bugs with the toggle theme button.

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="dark">
       <body className="w-screen h-screen bg-white dark:bg-black text-black dark:text-white">
         <div className="w-full h-full grid grid-rows-[4rem_auto]">
           {/* Navbar */}
--- app/ui/ThemeSlider.tsx
+++ app/ui/ThemeSlider.tsx
@@ -1,27 +1,53 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Slider from "./Slider";
 
 export default function ThemeSlider() {
+  const [isClient, setIsClient] = useState(false);
+
+  function setDarkTheme() {
+    document.documentElement.classList.remove("light");
+    document.documentElement.classList.add("dark");
+  }
+
+  function setLightTheme() {
+    document.documentElement.classList.remove("dark");
+    document.documentElement.classList.add("light");
+  }
+
   useEffect(() => {
-    document.documentElement.classList.add(localStorage.theme);
+    setIsClient(true);
+
+    // Detect system preference and set the initial theme to it
+    if (typeof localStorage.theme === "undefined") {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        localStorage.theme = "dark";
+      } else {
+        localStorage.theme = "light";
+      }
+    }
+
+    // Set the theme from localStorage
+    if (localStorage.theme === "dark") {
+      setDarkTheme();
+    } else {
+      setLightTheme();
+    }
   }, []);
 
   // Prevent prerender error during build process
-  if (typeof window === "undefined") return null;
+  if (!isClient) return null;
 
   return (
     <Slider
       onClick={(checked) => {
         if (checked) {
           localStorage.theme = "light";
-          document.documentElement.classList.remove("dark");
-          document.documentElement.classList.add("light");
+          setLightTheme();
         } else {
           localStorage.theme = "dark";
-          document.documentElement.classList.remove("light");
-          document.documentElement.classList.add("dark");
+          setDarkTheme();
         }
       }}
       defaultChecked={localStorage.theme === "light"}
