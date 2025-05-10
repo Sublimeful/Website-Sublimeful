9e0f43120d8b6a0fff7fd0d793863bc91f6b1772
Sublimeful <Qiangwu2000@gmail.com>
2025-05-09 19:15:27 -0500

    Formatting

--- app/games/wordscope/page.tsx
+++ app/games/wordscope/page.tsx
@@ -40,8 +40,8 @@ export default function Page() {
       if (guess[i] === todaysWord[i]) {
         rightPositions++;
       }
-      todaysWordCounter[todaysWord[i]] = (todaysWordCounter[todaysWord[i]] ??
-        0) + 1;
+      todaysWordCounter[todaysWord[i]] =
+        (todaysWordCounter[todaysWord[i]] ?? 0) + 1;
       guessCounter[guess[i]] = (guessCounter[guess[i]] ?? 0) + 1;
     }
 
